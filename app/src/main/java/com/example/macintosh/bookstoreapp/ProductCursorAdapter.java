7ecc6a02028b1c3727ca4ef338bfe9df7658b3c9
awaqar <azzumwaqar@gmail.com>
2018-05-28 11:31:18 +0100

    Sale button implemented

--- app/src/main/java/com/example/macintosh/bookstoreapp/ProductCursorAdapter.java
+++ app/src/main/java/com/example/macintosh/bookstoreapp/ProductCursorAdapter.java
@@ -1,8 +1,12 @@
 package com.example.macintosh.bookstoreapp;
 
 import android.app.Application;
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +15,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.macintosh.bookstoreapp.data.ProductContract.ProductEntry;
+
+import java.lang.annotation.RetentionPolicy;
 
 /**
  * Created by macintosh on 18/05/2018.
@@ -30,35 +36,57 @@ public class ProductCursorAdapter extends CursorAdapter {
         return LayoutInflater.from(context).inflate(R.layout.list_item, viewGroup, false);    }
 
     @Override
-    public void bindView(View view, Context context, final Cursor cursor) {
-        TextView bookNameTxtView = view.findViewById(R.id.productnameTextView);
-        TextView qtyTxtView = view.findViewById(R.id.quantityTextView);
-        TextView priceTxtView = view.findViewById(R.id.priceBtn);
+    public void bindView(View view, final Context context, final Cursor cursor) {
+        final TextView bookNameTxtView = view.findViewById(R.id.productnameTextView);
+        final TextView qtyTxtView = view.findViewById(R.id.quantityTextView);
+        final TextView priceTxtView = view.findViewById(R.id.priceBtn);
         TextView stockTxtView = view.findViewById(R.id.stock_statustextview);
+        TextView suppNameTxtView = view.findViewById(R.id.edit_supp_name);
+        TextView suppPhoneTxtView = view.findViewById(R.id.edit_supp_phone);
+
         TextView buy_TxtView = view.findViewById(R.id.priceBtn);
+        buy_TxtView.setTag(cursor.getPosition());
 
         int indexBookNameCol = cursor.getColumnIndex(ProductEntry.NAME);
         int indexPriceCol = cursor.getColumnIndex(ProductEntry.PRICE);
         int indexQtyCol = cursor.getColumnIndex(ProductEntry.QUANTITY);
         int indexStockStatCol = cursor.getColumnIndex(ProductEntry.STOCK_STATUS);
+        int indexSuppNameCol = cursor.getColumnIndex(ProductEntry.SUPPLIER_NAME);
+        int indexSuppPhoneCol = cursor.getColumnIndex(ProductEntry.SUPPLIER_PHONE_NUMBER);
 
         String name_book = cursor.getString(indexBookNameCol);
         int qty = cursor.getInt(indexQtyCol);
         double price_book = cursor.getDouble(indexPriceCol);
         int stock_status = cursor.getInt(indexStockStatCol);
+        final String supplierName = cursor.getString(indexSuppNameCol);
+        final int supplierPhone = cursor.getInt(indexSuppPhoneCol);
 
         bookNameTxtView.setText(name_book);
         qtyTxtView.setText(String.valueOf(qty));
         priceTxtView.setText(String.valueOf(price_book));
         stockTxtView.setText((stock_status==1?"IN  STOCK":"OUT OF STOCK"));
 
+        final Uri currentProductUri = ContentUris.withAppendedId(ProductEntry.CONTENT_URI,cursor.getInt(cursor.getColumnIndex(ProductEntry.PRODUCT_ID)));
+
+
         buy_TxtView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(view.getContext(), "button clicked" + cursor.getPosition() , Toast.LENGTH_SHORT).show();
+//                int pos = (Integer) view.getTag();
+//                Toast.makeText(context, ""+pos, Toast.LENGTH_SHORT).show();
+//                Log.v("CURSOR ID ",""+ pos);
+                ContentValues values = new ContentValues();
+                int newqty = cursor.getInt(cursor.getColumnIndex(ProductEntry.QUANTITY));
+//                before I put the values in contentvalues I need to check what is the quantity in the database stored
+                if(newqty>0){
+                    newqty--;
+                    if(newqty==0) values.put(ProductEntry.STOCK_STATUS,ProductEntry.OUT_OF_STOCK);
+                }
+
+                values.put(ProductEntry.QUANTITY,newqty);
+                context.getContentResolver().update(currentProductUri,values,null,null);
 
             }
         });
-
     }
 }
